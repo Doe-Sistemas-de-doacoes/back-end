15a948fb182ded8161361da1a4613ba51291b65f
DavidChavess <davi.ch.fe@gmail.com>
2021-09-01 20:03:54 -0300

    first commit

---
+++ src/test/java/com/labes/doe/DoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labes.doe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
